91b9428235cff378e2e8c5836edf7f0a533723fe
Julio A. Mistral <jmistral@grio.com>
2013-02-24 22:20:22 -0800

    Create Rails application skeleton with rspec support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecikeepRails::Application.config.secret_token = 'c69ca9fee519e383b9d508e1c57d811b44c507ef5cd1cd340d05b11fee434bd35df70c39a8128004c87fec8a5de666a4200fc7529424d69ba65dfc45a0f56d36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RecikeepRails::Application.config.session_store :cookie_store, key: '_recikeep_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RecikeepRails::Application.config.session_store :active_record_store
