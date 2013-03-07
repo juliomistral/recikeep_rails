06a3b38ead3b4aa419e04f8f3bf205393530f65c
Julio A. Mistral <jmistral@grio.com>
2013-03-06 21:34:46 -0800

    Revamp user model to contain last name and first name

--- db/migrate/20130226055618_create_users.rb
+++ db/migrate/20130226055618_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_hash
       t.string :password_salt
+      t.string :first_name
+      t.string :last_name
       t.timestamps
     end
   end
