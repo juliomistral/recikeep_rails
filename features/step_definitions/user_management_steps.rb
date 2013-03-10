3349e59ab79b321dc6de83b578c9aabdd5137510
Julio A. Mistral <jmistral@grio.com>
2013-03-10 15:48:36 -0700

    Add an acceptance test for signing up via the signup page

---
+++ features/step_definitions/user_management_steps.rb
@@ -0,0 +1,34 @@
+Given /^I am an anonymous user$/  do
+  nil
+end
+
+Given /^I'm on the the "(.*?)" page$/  do |arg1|
+  visit('/signup')
+end
+
+When /^I fill in signup form with the following data$/  do |table|
+  values = table.hashes[0]
+
+  fill_in('user_email', :with => values['Email'])
+  fill_in('user_password', :with => values['Password'])
+  fill_in('user_password_confirmation', :with => values['Confirm Password'])
+
+  click_link_or_button('Sign up')
+end
+
+Then /^I'm redirected to the welcome page$/  do
+  page.current_path.should == root_path
+end
+
+Then(/^a new user should exist with email "(.*?)"$/) do |email|
+  User.find_by_email(email).should_not == nil
+end
+
+Then /^I see a message saying I signed up correctly$/  do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see the welcome message "(.*?)"$/  do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
