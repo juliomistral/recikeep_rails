6095d9adc25d64565ae5b9c86dd389973305cf90
Julio A. Mistral <jmistral@grio.com>
2013-03-19 19:50:29 -0700

    Replace double-quotes with single-quotes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
   def setup_valid_new_user
-    @user = double("new user")
+    @user = double('new user')
     @user.stub(:id).and_return('user_id')
     User.any_instance.stub(:save).and_return(@user)
   end
@@ -12,7 +12,7 @@ describe UsersController do
   end
 
   def setup_existing_user
-    @user = double("existing user")
+    @user = double('existing user')
     @user.stub(:id).and_return('id')
     @controller.stubs(:current_user).returns(@user)
   end
@@ -22,54 +22,54 @@ describe UsersController do
     User.rspec_reset
   end
 
-  it "new action should render new template" do
+  it 'new action should render new template' do
     get :new
     response.should render_template(:new)
   end
 
-  it "create action should render new template when model is invalid" do
+  it 'create action should render new template when model is invalid' do
     setup_invalid_new_user
     post :create
     response.should render_template(:new)
   end
 
-  it "create action should redirect when model is valid" do
+  it 'create action should redirect when model is valid' do
     setup_valid_new_user
     post :create
     response.should redirect_to(root_url)
     session['user_id'].should == assigns['user'].id
   end
 
-  it "edit action should redirect when not logged in" do
-    get :edit, :id => "ignored"
+  it 'edit action should redirect when not logged in' do
+    get :edit, :id => 'ignored'
     response.should redirect_to(login_url)
   end
 
-  it "edit action should render edit template" do
+  it 'edit action should render edit template' do
     setup_valid_new_user
     @controller.stubs(:current_user).returns(@user)
-    get :edit, :id => "ignored"
+    get :edit, :id => 'ignored'
     response.should render_template(:edit)
   end
 
-  it "update action should redirect when not logged in" do
-    put :update, :id => "ignored"
+  it 'update action should redirect when not logged in' do
+    put :update, :id => 'ignored'
     response.should redirect_to(login_url)
   end
 
-  it "update action should render edit template when user is invalid" do
+  it 'update action should render edit template when user is invalid' do
     setup_existing_user
     @user.stub(:update_attributes).and_return(nil)
 
-    put :update, :id => "ignored"
+    put :update, :id => 'ignored'
     response.should render_template(:edit)
   end
 
-  it "update action should redirect when user is valid" do
+  it 'update action should redirect when user is valid' do
     setup_existing_user
     @user.stub(:update_attributes).and_return(@user)
 
-    put :update, :id => "ignored"
+    put :update, :id => 'ignored'
     response.should redirect_to(root_url)
   end
 end
