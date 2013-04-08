e92afb6b2582606a0c097b06eb2ff4a453b67328
Julio A. Mistral <jmistral@grio.com>
2013-04-08 08:15:58 -0700

    Add feature tests for creating recipes and anonymous user visits
    This commit also contains significant refactoring of the step definitions to pull commonly used functionality into an extension loaded by World.

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -1,18 +1,20 @@
-PATH_MAPPINGS = {
-    'Sign up' => '/signup',
-    'Home' => '/',
-    'Log in' => '/login'
-}
-
 Then /^I'm redirected to the "(.*?)" page$/  do |page_name|
-  page.current_path.should == PATH_MAPPINGS[page_name]
+  verify_current_page_is(page_name)
 end
 
-Given /^I'm on the the "(.*?)" page$/  do |page_name|
-  page_to_visit = PATH_MAPPINGS[page_name]
-  if page_to_visit != nil
-    visit(page_to_visit) unless page_to_visit == nil
-  else
-    raise "Page '%s' doesn't have mapping setup in the testing steps" % page_name
-  end
+When /^I go to the "(.*?)" page$/ do |page_name|
+  visit_page(page_name)
 end
+
+Given /^I'm on the "(.*?)" page$/  do |page_name|
+  visit_page(page_name)
+end
+
+Given /^a logged in user$/ do
+  login
+end
+
+Then /^the user is redirected to the "(.*?)" page$/ do |page_name|
+  verify_current_page_is(page_name)
+end
+
